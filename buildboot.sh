989e61060d26a2a6cd10cefde29b10a322ef2c7b
Sebastian Palarus <s_palarus@googlemail.com>
2020-06-28 00:09:16 +0200

    move to karaf-base

--- buildboot.sh
+++
@@ -1,7 +0,0 @@
-/opt/karaf/bin/karaf run & 
-sleep 333
-/opt/karaf/bin/client -r 93  "shell:sleep 1;"
-sleep 93
-/opt/karaf/bin/client -r 3  -l 2 -b < /tmp/build.commands
-/opt/karaf/bin/client -r 3  -l 2 -u karaf -p karaf "shutdown -f;" 
-sleep 33 
